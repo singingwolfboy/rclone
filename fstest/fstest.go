a962efba8bfb284c4affdd58b7611b8f2bfaa787
Fabian Möller <fabianm88@gmail.com>
2018-11-02 13:16:13 +0100

    fstest: remove WinPath from fstest.Item

--- fstest/fstest.go
+++ fstest/fstest.go
@@ -86,7 +86,6 @@ type Item struct {
 	Hashes  map[hash.Type]string
 	ModTime time.Time
 	Size    int64
-	WinPath string
 }
 
 // NewItem creates an item from a string content
@@ -179,7 +178,6 @@ func NewItems(items []Item) *Items {
 	// Fill up byName
 	for i := range items {
 		is.byName[Normalize(items[i].Path)] = &items[i]
-		is.byNameAlt[Normalize(items[i].WinPath)] = &items[i]
 	}
 	return is
 }
@@ -194,7 +192,6 @@ func (is *Items) Find(t *testing.T, obj fs.Object, precision time.Duration) {
 	}
 	if i != nil {
 		delete(is.byName, i.Path)
-		delete(is.byName, i.WinPath)
 		i.Check(t, obj, precision)
 	}
 }
@@ -212,21 +209,14 @@ func (is *Items) Done(t *testing.T) {
 // makeListingFromItems returns a string representation of the items
 //
 // it returns two possible strings, one normal and one for windows
-func makeListingFromItems(items []Item) (string, string) {
-	nameLengths1 := make([]string, len(items))
-	nameLengths2 := make([]string, len(items))
+func makeListingFromItems(items []Item) string {
+	nameLengths := make([]string, len(items))
 	for i, item := range items {
-		remote1 := Normalize(item.Path)
-		remote2 := remote1
-		if item.WinPath != "" {
-			remote2 = item.WinPath
-		}
-		nameLengths1[i] = fmt.Sprintf("%s (%d)", remote1, item.Size)
-		nameLengths2[i] = fmt.Sprintf("%s (%d)", remote2, item.Size)
+		remote := Normalize(item.Path)
+		nameLengths[i] = fmt.Sprintf("%s (%d)", remote, item.Size)
 	}
-	sort.Strings(nameLengths1)
-	sort.Strings(nameLengths2)
-	return strings.Join(nameLengths1, ", "), strings.Join(nameLengths2, ", ")
+	sort.Strings(nameLengths)
+	return strings.Join(nameLengths, ", ")
 }
 
 // makeListingFromObjects returns a string representation of the objects
@@ -283,7 +273,7 @@ func CheckListingWithRoot(t *testing.T, f fs.Fs, dir string, items []Item, expec
 	var err error
 	var retries = *ListRetries
 	sleep := time.Second / 2
-	wantListing1, wantListing2 := makeListingFromItems(items)
+	wantListing := makeListingFromItems(items)
 	gotListing := "<unset>"
 	listingOK := false
 	for i := 1; i <= retries; i++ {
@@ -293,7 +283,7 @@ func CheckListingWithRoot(t *testing.T, f fs.Fs, dir string, items []Item, expec
 		}
 
 		gotListing = makeListingFromObjects(objs)
-		listingOK = wantListing1 == gotListing || wantListing2 == gotListing
+		listingOK = wantListing == gotListing
 		if listingOK && (expectedDirs == nil || len(dirs) == len(expectedDirs)) {
 			// Put an extra sleep in if we did any retries just to make sure it really
 			// is consistent (here is looking at you Amazon Drive!)
@@ -312,7 +302,7 @@ func CheckListingWithRoot(t *testing.T, f fs.Fs, dir string, items []Item, expec
 			doDirCacheFlush()
 		}
 	}
-	assert.True(t, listingOK, fmt.Sprintf("listing wrong, want\n  %s or\n  %s got\n  %s", wantListing1, wantListing2, gotListing))
+	assert.True(t, listingOK, fmt.Sprintf("listing wrong, want\n  %s got\n  %s", wantListing, gotListing))
 	for _, obj := range objs {
 		require.NotNil(t, obj)
 		is.Find(t, obj, precision)
